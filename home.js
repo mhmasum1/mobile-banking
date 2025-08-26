5ad1d1607aa8280f74c7dc83ad9d7c07c38a342d
mhmasum1 <mhmasum231@gmail.com>
2025-08-26 18:23:55 +0600

    add pin and bank account number validation

--- home.js
+++ home.js
@@ -1,3 +1,5 @@
+const validPin = 12345;
+
 document.getElementById("add-money-btn").addEventListener('click', function (event) {
     event.preventDefault();
     const aviableBalance = parseInt(document.getElementById("aviable-balance").innerText);
@@ -6,6 +8,16 @@ document.getElementById("add-money-btn").addEventListener('click', function (eve
     const addAmount = parseInt(document.getElementById("add-amount").value);
     const addPin = document.getElementById("add-pin").value;
     console.log(aviableBalance, selectBank, bankNumber, addAmount, addPin)
+
+    if (bankNumber.length !== 11 || NaN(bankNumber)) {
+        alert("Please provided correct account number")
+        return;
+    }
+    if (addPin !== validPin) {
+        alert("Please provide valid pin");
+        return;
+    }
     const totalAmount = aviableBalance + addAmount;
     document.getElementById("aviable-balance").innerText = totalAmount;
+
 })
